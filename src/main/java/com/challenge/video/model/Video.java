557abd79505bde83eee35c0e200c5c5786260cfe
Jorge Meireles <jorgentoniomeireles@gmail.com>
2022-09-15 19:53:01 -0300

    Classe Video criado

---
+++ src/main/java/com/challenge/video/model/Video.java
@@ -0,0 +1,15 @@
+package com.challenge.video.model;
+
+
+
+public class Video {
+    private Long id;
+    private String titulo;
+    private String descricao;
+    private String url;
+
+
+
+
+
+}
