87c927cb06d66857336932afc5998af43660fd90
Jorge Meireles <jorgentoniomeireles@gmail.com>
2022-09-15 19:44:31 -0300

    projeto criado

---
+++ src/test/java/com/challenge/video/VideoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.challenge.video;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
